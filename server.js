692f2da117f4f16293d8d2f9fc8ee9924504fdb4
thomsaab <sabu.thomas@gmail.com>
2018-05-12 10:25:38 -0400

    homework week11-express - updated

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+require('./routing/api-routes.js')(app); 
+require('./routing/html-routes.js')(app);
 
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
